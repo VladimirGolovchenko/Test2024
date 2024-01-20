12a9ffcaeee80381cd9da170ec50ed6340021f6a
Vladimir <58787403+VladimirGolovchenko@users.noreply.github.com>
2024-01-21 02:29:40 +0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,73 @@
+// <!-- <script>
+// 	function jivo_onLoadCallback() {
+// 		setTimeout(() => {
+// 			jivo_api.setUserToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjEifQ.v6cHE6GB0t0t4J42ECnb8il8N4X40nLnR2RLMobj8Kc"); 
+// 		}, "1000");
+// 	}
+// </script> -->
+	
+// <!-- 
+// <script> 
+//     document.addEventListener("DOMContentLoaded", () => {
+// 	    	setTimeout(() => {
+//    			jivo_config.chat_mode = 'online';
+// 			window.jivo_init();
+// 		}, "1000");
+//   });
+// </script> -->
+
+// <!-- <script>
+// 	function jivo_onLoadCallback() {
+// 		setTimeout(() => {
+// 	   		jivo_api.setUserToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjEifQ.v6cHE6GB0t0t4J42ECnb8il8N4X40nLnR2RLMobj8Kc");
+// 		}, "1000");
+// 	}
+// </script> -->
+
+// <!-- <script>
+//   function jivo_onLoadCallback() {
+//     //скрываем стандартную кнопку Jivo
+//     const jivo_custom_widget = true;
+//   }
+// </script> -->
+
+// <!-- <button class="button1" onclick="jivo_api.open();">Support</button>
+
+// <script>
+//   function jivo_onOpen() {
+//     button1.style.display = 'none';
+//   }
+// </script>
+
+// <script>
+//   function jivo_onClose() {
+//     button1.style.display = 'block';
+//   }
+// </script>
+//   -->
+ 
+
+// <!-- <style>
+// 	jdiv[class^="header_"] > jdiv {
+//     		border-top-right-radius: 12px!important;
+// 	}
+
+// 	jdiv[class^="header_"] jdiv[class^="leaf_"] {
+// 	    display: none !important;
+// 	}
+// </style>
+//  -->
+// <!-- <script>
+// 	window.jivo_chat_page = true;
+// </script>
+	
+// <style>	
+// 	body jdiv[class^="globalClass"] > jdiv[class^="mobileContainer_"] {
+// 		position: absolute !important;
+
+// 		width: 50% !important;
+// 		height: 600px !important;
+// 		margin-top: 50px !important;
+// 		margin-left: 200px !important;
+// 	}
+// </style> -->
